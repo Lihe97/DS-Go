c3a529a0191c9766709888940f8ab802118d7a5c
Lihe97 <lihe.kansas@gmail.com>
2020-03-24 19:10:31 +0800

    3.24 19:10

---
+++ Sort/单链表直接插入排序.go
@@ -0,0 +1,45 @@
+package main
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+func insertionSortList(head *ListNode) *ListNode {
+
+	if head == nil{
+		return head
+	}
+	res := &ListNode{
+		Val:  11,
+		Next: nil,
+	}
+	cur := res
+	cur.Next = head
+	head = head.Next
+	cur.Next.Next = nil
+	for head!= nil{
+		cur = res
+		for cur.Next != nil && head != nil{
+			if head.Val <= cur.Next.Val{
+				Next := head.Next
+				head.Next = cur.Next
+				cur.Next = head
+				head = Next
+				break
+			}else{
+				cur = cur.Next
+			}
+		}
+		if cur.Next == nil{
+			Next := head.Next
+			cur.Next = head
+			head.Next = nil
+			head = Next
+		}
+
+	}
+
+	return res.Next
+}
+func main() {
+	
+}
